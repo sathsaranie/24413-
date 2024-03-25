76806f4c872549a259872afe72697b9f27a6dbc7
sathsaranie <dimalkasathsaranie@gmail.com>
2024-03-25 10:29:59 +0530

    commit again

---
+++ ConsoleApp4/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public double basicSalary { get; set; }
+
+
+
+        public void show()
+        {
+
+            Console.WriteLine("name:" + firstName + lastName + "salary:" + basicSalary);
+        }
+              
+    }
+
+}
