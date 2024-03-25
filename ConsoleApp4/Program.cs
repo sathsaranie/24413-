e3d2f7636c90f2cccf9e98fd8b5c66f64495b002
sathsaranie <dimalkasathsaranie@gmail.com>
2024-03-25 10:15:40 +0530

    commit again

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        24413
+        int[] numbers = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        double total = 0;
+        double average= 0;
+
+        foreach (int i in numbers)
+        {
+            total += i;
+        }
+        average = total / numbers.Length;
+        Console.WriteLine("total =" + total +" , average="+average); 
+       
+
     }
 }
